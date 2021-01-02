1dceb2f9ae7245f875c1d513dead8dd3d3cc07f1
Joe <fernettejs@gmail.com>
2021-01-02 03:28:01 -0500

    fix repeat and button bugs

--- affirmation_api/main.js
+++ affirmation_api/main.js
@@ -16,14 +16,6 @@ async function getAff() {
       speak();
 }
 
-  
 getAff();
-
-// if ('speechSynthesis' in window) {
-//     // Speech Synthesis supported 🎉
-//    }else{
-//      // Speech Synthesis Not Supported 😣
-//      alert("Sorry, your browser doesn't support text to speech!");
-//    }
 
 
