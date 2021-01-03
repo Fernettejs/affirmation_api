7abdc25181f8e098efcb039ef8a3728863c8be31
Joe <fernettejs@gmail.com>
2021-01-03 03:26:25 -0500

    Fix CORS error and center text

--- affirmation_api/main.js
+++ affirmation_api/main.js
@@ -1,14 +1,17 @@
-const aff_url = "https://www.affirmations.dev";
+if ('speechSynthesis' in window) {
+  // Speech Synthesis supported 🎉
+ }else{
+   // Speech Synthesis Not Supported 😣
+   alert("Sorry, your browser doesn't support text to speech!");
+ }
+
+const aff_url = "https://cors-anywhere.herokuapp.com/https://www.affirmations.dev"; // https://github.com/Rob--W/cors-anywhere
 async function getAff() {
     const response = await fetch(aff_url);
     const data = await response.json();
     const { affirmation } = data;
 
     document.getElementById('affirmation').textContent = affirmation;
-    
-    // var msg = new SpeechSynthesisUtterance();
-    // msg.text = affirmation;
-    // window.speechSynthesis.speak(msg);
 
     function speak() {
         speechSynthesis.speak(new SpeechSynthesisUtterance(affirmation));
@@ -17,5 +20,6 @@ async function getAff() {
 }
 
 getAff();
+
 
 
